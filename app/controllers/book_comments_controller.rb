a939fb7592591094201ba4a7066edb3b11a0a16b
determinate1209 <determinate1209@gmail.com>
2022-04-19 08:55:23 +0000

    非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,17 @@
 class BookCommentsController < ApplicationController
   def create
-     book = Book.find(params[:book_id])
+     @books = Book.all
+     @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    if comment.save
-      redirect_to request.referer
-    else
-      redirect_to request.referer
-    end
+    comment.book_id = @book.id
+    comment.save
   end
+  
   def destroy
+    refroute = Rails.application.routes.recognize_path(request.referrer)
+    @book = Book.find(refroute[:id])
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    
   end
   
   

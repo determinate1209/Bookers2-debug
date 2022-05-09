c8d3a279833634fcd534a0da0c1c99cdf7b54b25
determinate1209 <determinate1209@gmail.com>
2022-05-09 04:39:58 +0000

    [finish]8b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,12 @@ before_action :correct_user, only: [:edit, :update]
     @book = Book.new
     @today_book =  @books.created_today
     @yesterday_book = @books.created_yesterday
-    @this_week_book = @books.created_this_week
-    @last_week_book = @books.created_last_week
+    @two_days_ago_book = @books.created_two_days_ago
+    @three_days_ago_book = @books.created_three_days_ago
+    @four_days_ago_book = @books.created_four_days_ago
+    @five_days_ago_book = @books.created_five_days_ago
+    @six_days_ago_book = @books.created_six_days_ago
+    
     
   end
 
